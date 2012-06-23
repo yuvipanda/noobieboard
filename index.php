79e18e233186968980dfeb8413011fe322d6cc97
YuviPanda <yuvipanda@gmail.com>
2012-06-23 16:26:06 +0530

    Setup to use Twig

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 require_once "vendor/autoload.php";
 
-$app = new Slim(array(
-));
+$app = new Slim();
+
+// Use Twig for templating
+require 'vendor/slim/extras/Views/TwigView.php';
+$app->view('TwigView');
 
 require_once "urls.php";
 require_once "controllers.php";
